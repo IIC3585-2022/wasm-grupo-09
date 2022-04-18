f27386f4f9655d348d52d913b757e31278c0276a
mjespinoza <mjespinoza2@uc.cl>
2022-04-18 17:27:42 -0400

    fix return of partition

--- src/partition/main.c
+++ src/partition/main.c
@@ -1,18 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int len(int* arr){
-    int size_arr = (int) sizeof(arr);
-    printf("arr size %i\n", size_arr);
-    int len  = (int) (size_arr/sizeof(arr[0]));
-    return len;
-};
+typedef struct subset
+{
+    int len;
+    int* subset;
+} Subset;
+
+
+typedef struct answer
+{
+    int partition;
+    Subset** subset;
+    char* msg;
+} Answer;
+
+
 int accumulate(int* S, int n){
     int sum = 0;
     for(int i = 0; i<n; i++){
         sum += S[i];
     }
     return sum;
+};
+
+void clean(Answer* ans){
+    for(int i=0; i<3; i++){
+        free(ans->subset[i]->subset);
+        free(ans->subset[i]);
+    }
+    free(ans->subset);
+    free(ans);
+
 };
 
 // codigo obtenido de: https://www.techiedelight.com/3-partition-problem-extended-print-all-partitions/
@@ -45,40 +64,63 @@ int isSubsetExists(int* S, int n, int a, int b, int c, int* arr){
     return A || B || C;
 };
 
-int partition(int* S, int n){
-    
+Answer* partition(int* S, int n){
     int sum = accumulate(S, n);
     int arr[n];
     int result = (n>=3) && !(sum%3) && isSubsetExists(S, n-1, (int) sum/3, (int) sum/3,(int) sum/3, arr);
-    if(!result){
-        printf("3-partition of set is not posible\n");
-        return 0;
-    }
-    for(int i=0; i<3; i++){
-        printf("Partition %i:", i);
-        for(int j =0; j<n; j++){
-            if(arr[j]== i+1){
-                printf(" %i", S[j]);
+    Answer* answer = malloc(sizeof(Answer));
+    answer->partition = result;
+    answer->subset = calloc(3, sizeof(Subset*));
+    if(result){
+        for(int i=0; i<3; i++){
+            answer->subset[i] = malloc(sizeof(Subset));
+            for(int j =0; j<n; j++){
+                if(arr[j]== i+1){
+                    answer->subset[i]->len++;
+                }
             }
-        }
-        printf("\n");
+            answer->subset[i]->subset = calloc(answer->subset[i]->len, sizeof(int));
+            
+            int k=0;
+            for(int j =0; j<n; j++){
+                if(arr[j]== i+1){
+                    answer->subset[i]->subset[k] = S[j];
+                    k++;
+                   
+                }
 
-
+            }
+            
+        }  
+       
     }
-    return 1;
+    answer->msg = "3-partition of set is not posible\n";
+    return answer;
+
     
 
 }
 
 
 int main(){
-    int S[7] = {7, 3, 2, 1, 5, 4, 8};
+    int S[7] = {7, 3, 2, 1, 5,4, 8};
     int size_arr = (int) sizeof(S);
-    printf("arr size %i\n", size_arr);
     int n  = (int) (size_arr/sizeof(S[0]));
-   
-    printf("%i\n", n);
-    partition(S,n);
+    Answer* ans = partition(S,n);
+    if(!ans->partition){
+        printf("%s", ans->msg);
+
+    }else{
+        for(int i = 0; i<3; i++){
+            printf("partition %i:", i);
+            for(int j=0; j<ans->subset[i]->len; j++){
+                printf(" %i ",ans->subset[i]->subset[j]);
+            }
+            printf("\n");
+        }
+        clean(ans);
+    }
+    
     return 0;
 
 }
